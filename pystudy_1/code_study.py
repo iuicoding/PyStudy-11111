0130a6ff938ef6607dd9678acc6eb776e48fca60
sinooooo <sw0411@icloud.com>
2022-06-15 15:46:04 +0900

    update

--- pystudy_1/code_study.py
+++ pystudy_1/code_study.py
@@ -1,1 +1,9 @@
 print('hello world!');
+#괜찮겠어? 난 멈추는 법을 모르는 컴맹인데.
+print("I want to go home")
+print('어쩌라고')
+print("'니 새끼손가락 하나가 부러지는 것 보다 내 갈비뼈가 몽땅 나가는 게 더 나으니까.'라고 말했습니다")
+print("사탕"+"묵을래?")
+print("뿡"*3)
+string=input("안녕이라고 내게 말하지 마>")
+print(string)
